f1ea3a28f50a8f78b9b64ddc7b3af7626f02ddf8
Vinh Pham <phamv1@unlv.nevada.edu>
2021-11-15 15:10:26 -0800

    finish requirement

--- move.js
+++ move.js
@@ -6,7 +6,65 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, handelImageAnimation) {
+
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        character.style.left = x + 'px';
+        character.style.bottom = y + 'px';
+
+        function moveCharacter() {
+            if(direction === 'west') {
+                x--;
+            } else if (direction === 'north') {
+                y++;
+            } else if (direction === 'east') {
+                x++;
+            } else if (direction === 'south') {
+                y--;
+            }
+
+            character.style.left = x + 'px';
+            character.style.bottom = y + 'px';
+        }
+        setInterval(moveCharacter, 1)
+
+        document.addEventListener('keydown', function(e) {
+
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft') {
+                direction = 'west';
+            }
+            if(e.key === 'ArrowRight') {
+                direction = 'east';
+            } 
+            if(e.key === 'ArrowUp') {
+                direction = 'north';
+            }
+            if(e.key === 'ArrowDown') {
+                direction = 'south';
+            }
+
+            if(handelImageAnimation) {
+                handelImageAnimation(direction);
+            }
+            
+        })
+        
+        document.addEventListener('keyup', function(e) {
+            direction = null;
+            if(handelImageAnimation) {
+                handelImageAnimation(direction);
+            }
+        })
+    }
+   
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
